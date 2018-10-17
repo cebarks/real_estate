f596dc18b80702a47a0abc578ed1b70c9261c59d
Anten Skrabec <cebarks@gmail.com>
2018-10-17 09:59:41 -0600

    Iteration 4 implemenation

--- lib/house.rb
+++ lib/house.rb
@@ -24,4 +24,25 @@ class House
       room.catagory == catagory
     end
   end
+
+  def price_per_square_foot
+    (@price / area.to_f).round(2)
+  end
+
+  def rooms_sorted_by_area
+    @rooms.sort_by do |room|
+      room.area
+    end
+  end
+
+  def rooms_by_category
+    types = @rooms.map do |room|
+      room.catagory
+    end
+    rooms_hash = {}
+    types.uniq.each do |type|
+      rooms_hash[type] = @rooms.select {|room| room.catagory == type}
+    end
+    rooms_hash
+  end
 end
