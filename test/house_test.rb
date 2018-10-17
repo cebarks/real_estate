d8ea6c42a10d615a50df6ea4d8843c66030fcc67
Anten Skrabec <cebarks@gmail.com>
2018-10-17 09:53:08 -0600

    Iteration 4 testing

--- test/house_test.rb
+++ test/house_test.rb
@@ -35,19 +35,41 @@ class RoomTest < Minitest::Test
   end
 
   def test_it_can_return_rooms_by_catagory
-    @house.add_room(@room_1)
-    @house.add_room(@room_2)
-    @house.add_room(@room_3)
-    @house.add_room(@room_4)
+    add_rooms
     assert_equal [@room_1, @room_2], @house.rooms_from_catagory(:bedroom)
     assert_equal [@room_4], @house.rooms_from_catagory(:basement)
   end
 
   def test_it_can_calculate_total_area
+    add_rooms
+    assert_equal 1900, @house.area
+  end
+
+  def test_it_can_calculate_price_per_square_foot
+    add_rooms
+    assert_equal 210.53, @house.price_per_square_foot
+  end
+
+  def test_it_can_sort_rooms_by_area
+    add_rooms
+    expected = [@room_1, @room_2, @room_3, @room_4]
+    assert_equal expected, @house.rooms_sorted_by_area
+  end
+
+  def test_it_can_return_rooms_by_catagory
+    add_rooms
+    expected = {
+      :bedroom => [@room_1, @room_2],
+      :living_room => [@room_3],
+      :basement => [@room_4]
+    }
+    assert_equal expected, @house.rooms_by_category
+  end
+
+  def add_rooms
     @house.add_room(@room_1)
     @house.add_room(@room_2)
     @house.add_room(@room_3)
     @house.add_room(@room_4)
-    assert_equal 1900, @house.area
   end
 end
